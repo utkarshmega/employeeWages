20046098eee6d74bfcf9871c89112375ae4afc7c
utkarshmega <utkarsheduc@gmail.com>
2020-09-22 23:51:09 +0530

    UC5 completed

--- src/com/employeewages/employeewages.java
+++ src/com/employeewages/employeewages.java
@@ -7,20 +7,25 @@ public class employeewages {
 		
 		Random rnd = new Random();
 		
-		int att = rnd.nextInt(3);
 		int wph=20;
 		int wh = 8;
-		int htwh = 8;
-		
-		switch(att)
+		int days=0, wages=0, totalwages=0;
+		for(int i=1;i<=20;i++)
 		{
-			case 1: System.out.println("Total full time daily wages = Rs."+ (wph*wh) +"/-");
-			break;
-			case 2: System.out.println("Total part-time daily wage = Rs." + (wph*htwh)+"/-");
-			break;
-			default: 	System.out.println("Employee is absent"); 
-				
+			int att = rnd.nextInt(2);
+			if(att == 1)
+			{
+				days++;
+				wages = wph * wh;
+			}
+			else {
+				wages = 0;
+			}
+			totalwages += wages ;
+			
 		}
+		System.out.println("Total days worked = "+days);
+		System.out.println("Total wage for the month is Rs."+totalwages+"/-");
 				
 
 	}
